e1fc443aae72315231b04469e978ef2b6a4d3a9a
Lana Chiad <email": null>
2017-05-31 16:46:10 -0400

    Create user proposal edit route, view, and method

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -10,10 +10,17 @@ class ProposalsController < ApplicationController
   def new
   end
 
-  def update
+  def edit
+    @proposal = Proposal.find(params[:id])
   end
 
-  def edit
+  def update
+    @proposal = Proposal.find(params[:id])
+    if @proposal.update(proposal_params)
+      redirect_to user_proposal_url
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -21,5 +28,10 @@ class ProposalsController < ApplicationController
     @proposal.destroy
 
     redirect_to root
+  end
+
+  private
+  def proposal_params
+    params.require(:proposal).permit(:summary, :hypothesis, :status)
   end
 end
