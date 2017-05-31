d6740a71c1f434af7482a21d315c1a57814ceac4
Carlos Grijalva <grijalva.carlos@gmail.com>
2017-05-31 14:27:23 -0400

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+resource :users do
+  resources :proposals do
+    resources :experiments do
+      resources :procedures do
+        reources :comments, :observations, only [:new, :create, :update, :edit, :delete]
+      end
+    end
+  end
 end
