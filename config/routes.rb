48cff2aa2196164ee7fbc19d3391f9aeb90b08ea
Carlos Grijalva <grijalva.carlos@gmail.com>
2017-05-31 15:05:01 -0400

    Create all restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,24 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-resource :users do
-  resources :proposals do
-    resources :experiments do
-      resources :procedures 
+  resource :users do
+    resources :proposals do
+      resources :comments do
+      end 
+        resources :experiments do
+          resources :observations do
+            resources :comments do
+            end
+          end
+          resources :comments do
+          end
+        end
+        resources :procedures do
+          resources :observations do
+              resources :comments do
+                end
+              end
+            end
     end
   end
 end
