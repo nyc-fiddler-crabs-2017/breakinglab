aceda78dd093619e02a7faa1bc1a5ebaf3b2d61f
Kiv Lo <email": null>
2017-05-31 15:54:14 -0400

    add delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    # @
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+  
 end
