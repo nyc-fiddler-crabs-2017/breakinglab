bfd3f0ccb603f8b335b9d5ce9e3a0d40dfcf80db
Lana Chiad <email": null>
2017-05-31 11:07:45 -0400

    Fix syntax

--- db/migrate/20170531143738_create_experiments.rb
+++ db/migrate/20170531143738_create_experiments.rb
@@ -2,6 +2,7 @@ class CreateExperiments < ActiveRecord::Migration[5.1]
   def change
     create_table :experiments do |t|
       t.string :status, null: false
+      t.references :experimenter, foreign_key: true, null: false
       t.references :proposal, foreign_key: true, null: false
       t.references :procedure, foreign_key: true, null: false
 
