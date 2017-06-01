a200d934eff23513946fc934e5e0bcc5f41b7065
Thomas Feng <email": null>
2017-06-01 12:12:51 -0400

    Enable creation of new experiments

---
+++ app/controllers/experiments_controller.rb
@@ -0,0 +1,61 @@
+class ExperimentsController < ApplicationController
+  include SessionsHelper
+  def new
+    @experiment = Experiment.new
+    @proposal = Proposal.find(params[:proposal_id])
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    # @proposal = Proposal.find_by(:id params[:proposal_id])
+    @experiment = current_user.experiments.new(experiment_params)
+
+    # @proposal.experiments()
+    if @experiment.save
+      redirect_to @proposal
+    else
+      render '/experiments/new'
+    end
+  end
+
+  def show
+    @experiment = Experiment.find(params[:id])
+  end
+
+  def edit
+    @experiment = Experiment.find(params[:id])
+    @proposal = @experiment.proposal
+    @user = User.find(params[:user_id])
+    if current_user != @experiment.experimenter
+      redirect_to @experiment
+    end
+  end
+
+  def update
+    @experiment = Experiment.find(params[:id])
+    if current_user == @experiment.experimenter
+      if @experiment.update(experiment_params)
+        redirect_to @experiment
+      else
+        render '/experiments/edit'
+      end
+    else
+      redirect_to @experiment
+    end
+  end
+
+  def destroy
+    @experiment = Experiment.find(params[:id])
+    if current_user == @experiment.experimenter
+      @experiment.destroy
+      redirect_to @experiment.proposal
+    else
+      redirect_to @experiment
+    end
+  end
+
+  private
+  def experiment_params
+    params.require(:experiment).permit(:title, :proposal_id)
+  end
+end
